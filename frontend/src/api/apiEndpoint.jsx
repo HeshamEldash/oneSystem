ea1e73caa6735752f5ea83809faf6a40703f3fc9
HeshamEldash <heshamaldash44@gmail.com>
2023-03-06 13:28:35 +0000

    change copy42

--- frontend/src/api/apiEndpoint.jsx
+++ frontend/src/api/apiEndpoint.jsx
@@ -1,7 +1,7 @@
 let APIENDPOINT = "http://127.0.0.1:8000/app-api"
 
-if (import.meta.env.PROD){
-    APIENDPOINT = import.meta.env.BACKEND_ENDPOINT
+if (import.meta.env["PROD"]){
+    APIENDPOINT = import.meta.env["BACKEND_ENDPOINT"]
 }  
 
 export default APIENDPOINT
